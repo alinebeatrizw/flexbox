a839ee5d099f65fe2f5f047161ee71b8f387615d
alinebeatrizw <alinewille31@gmail.com>
2021-04-11 23:45:54 -0300

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).redy(function(){
+    $('.hamburger').click(function(){
+        $(this).toggleClass('ativa')
+        $( '.navegacao').toggleClass('ativa')
+    })
+})
+
+
+$("#hamburgere").click(function () {
+    $("#ex").hide();
+});
