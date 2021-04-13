c2aa584e70e2bd01a4ff1832467ed8ca5aa63fd7
alinebeatrizw <alinewille31@gmail.com>
2021-04-12 22:34:06 -0300

    add mudança de tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+
+//menu responsivo
+
 $(document).on('click', '#esconde', function(){ 
     $('#navegacao, #secaoPro, .botaoEsconde').css("display", "none"); 
     $('.botaoAparece').css("display", "block");     
@@ -8,3 +11,31 @@ $(document).on('click', '#aparece', function(){
     $('.botaoAparece').css("display", "none"); 
     $('.pro-secao').css("display", "flex");       
 });
+
+
+
+//muda tema
+
+//vai para tema escuro
+$(document).on('click', '#lua-acordada', function(){ 
+    $('#lua-acordada').css("display", "none"); 
+    $('#lua-dormindo').css("display", "block");
+
+
+    $('#main, .pro-secao, .barra-progresso').css("background", "var(--linear-bg-dark)");
+    
+});
+
+
+
+//volta ao tema colorido
+$(document).on('click', '#lua-dormindo', function(){ 
+    $('#lua-acordada').css("display", "block"); 
+    $('#lua-dormindo').css("display", "none");
+
+    $('#main, .pro-secao, .barra-progresso').css("background", "var(--linear-colorido)");
+   
+});
+
+
+
